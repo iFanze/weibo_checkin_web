55a1794547c844c174f89723c92a0945af8ce3a8
Meng Fanze <iFanze@outlook.com>
2017-03-03 19:36:22 +0800

    'area' model

--- www/weibo_checkin/models.py
+++ www/weibo_checkin/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Area(models.Model):
+    min_lat = models.DecimalField(max_digits=9, decimal_places=6)
+    max_lat = models.DecimalField(max_digits=9, decimal_places=6)
+    min_lon = models.DecimalField(max_digits=9, decimal_places=6)
+    max_lon = models.DecimalField(max_digits=9, decimal_places=6)
+    poi_count = models.IntegerField(default=0)
+    last_updated_at = models.DateTimeField()
